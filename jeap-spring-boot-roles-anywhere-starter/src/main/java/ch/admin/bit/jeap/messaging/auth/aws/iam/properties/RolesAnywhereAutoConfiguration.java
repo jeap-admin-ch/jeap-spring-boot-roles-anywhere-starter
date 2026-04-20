520ff99ca1917e7bad5de10c2aaa529e989e0dc3
Fabien Cerf <fabien.cerf@bit.admin.ch>
2026-04-20 13:55:16 +0200

    JEAP-6881: upgrade to spring boot 4

--- jeap-spring-boot-roles-anywhere-starter/src/main/java/ch/admin/bit/jeap/messaging/auth/aws/iam/properties/RolesAnywhereAutoConfiguration.java
+++ jeap-spring-boot-roles-anywhere-starter/src/main/java/ch/admin/bit/jeap/messaging/auth/aws/iam/properties/RolesAnywhereAutoConfiguration.java
@@ -1,19 +1,18 @@
 package ch.admin.bit.jeap.messaging.auth.aws.iam.properties;
 
 import ch.admin.bit.jeap.messaging.auth.aws.iam.AwsRolesAnywhereSessionOrchestrator;
+import ch.admin.bit.jeap.messaging.auth.aws.iam.IAMRolesAnywhereSessionsCredentialsProvider;
 import ch.admin.bit.jeap.messaging.auth.aws.iam.certs.CertLoader;
 import ch.admin.bit.jeap.messaging.auth.aws.iam.certs.PrivateKeyLoader;
-import ch.admin.bit.jeap.messaging.auth.aws.iam.util.IAMRolesAnywhereCredentialsProviderHolder;
-import ch.admin.bit.jeap.messaging.auth.aws.iam.IAMRolesAnywhereSessionsCredentialsProvider;
-import ch.admin.bit.jeap.messaging.auth.aws.iam.models.RolesAnywhereAuthContext;
 import ch.admin.bit.jeap.messaging.auth.aws.iam.mapper.RolesAnywhereAuthContextMapper;
+import ch.admin.bit.jeap.messaging.auth.aws.iam.models.RolesAnywhereAuthContext;
+import ch.admin.bit.jeap.messaging.auth.aws.iam.util.IAMRolesAnywhereCredentialsProviderHolder;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.micrometer.common.util.StringUtils;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
